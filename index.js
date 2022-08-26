552215aad960f9e259b714500daa8da01403d4ae
Lee Kopacz <lee_kopacz@yahoo.com>
2022-08-25 22:16:17 -0500

    commit tree 88328f16d7df3e4f960e2556f7c28cfba3388093

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b){
+    return a + b
+}
+function subtract(a, b){
+    return a - b
+}
+function multiply(a, b){
+    return a * b
+}
+function divide(a, b){
+    return a / b
+}
+
+let n = 10
+
+function increment(n){
+    return (n+1)
+}
+function decrement(n){
+    return (n-1)
+}
+
+function makeInt(a, b ){
+    return parseInt(a, 10) 
+}
+
+function preserveDecimal(a, b){
+return parseFloat(a, b)
+}
+parseInt("2.222", 10)
+parseFloat("80.23624")
+
+
